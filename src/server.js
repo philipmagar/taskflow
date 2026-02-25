c4f2b7c0f0550ee0b68227c260f8d916bcf18b1c
philip <philipmagar111@gmail.com>
2026-02-25 14:52:20 +0545

    added database connection

--- src/server.js
+++ src/server.js
@@ -1,9 +1,24 @@
 require("dotenv").config();
 
 const app = require("./app");
+const pool = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+  try{
+    const connection = await pool.getConnection();
+    console.log("Database connection established successfully");
+    connection.release();
+  
+  app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+  
+  });
+
+}catch(error){
+  console.error("Error connecting to the database:", error);
+  process.exit(1);
+}
+}
+startServer();
