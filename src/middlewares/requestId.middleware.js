a45407d64231de32ee5e8e7af9df2f66408c2c15
philip <philipmagar111@gmail.com>
2026-04-02 10:21:30 +0545

    feat: fix log rotation and complete checklist verification

---
+++ src/middlewares/requestId.middleware.js
@@ -0,0 +1,7 @@
+const { v4: uuidv4 } = require('uuid');
+const requestIdMiddleware = (req, res, next) => {
+    req.requestId = uuidv4();
+    res.setHeader('X-Request-ID', req.requestId);
+    next();
+}
+module.exports = requestIdMiddleware;
